b4c44d3b0d9eb813868f1bd4e60192e2a607279d
Nguyễn Hoàng Phong <138206785+Pwindy2611@users.noreply.github.com>
2024-05-19 19:00:37 +0700

    fix

--- src/app/translateLearn/page.tsx
+++ src/app/translateLearn/page.tsx
@@ -1,110 +1,110 @@
 import "../courseLearn/courseLearn.css"
 export default function translateLearn(){
     return(
-        <div class="content__container">
-        <input type="checkbox" id="content__checkbox" class="content__input" />
-        <div class="content-left hide-on-mobile hide-on-tablet">
-          <div class="flex__container header__container">
-            <a href="courseLearn.html" class="content-left__header">List 1</a>
-            <a href="" class="content-left__dropleft">
-              <label for="content__checkbox"
-                ><i class="content-left__icon fa-solid fa-angle-left"></i
+        <div className="content__container">
+        <input type="checkbox" id="content__checkbox" className="content__input" />
+        <div className="content-left hide-on-mobile hide-on-tablet">
+          <div className="flex__container header__container">
+            <a href="courseLearn.html" className="content-left__header">List 1</a>
+            <a href="" className="content-left__dropleft">
+              <label htmlFor="content__checkbox"
+                ><i className="content-left__icon fa-solid fa-angle-left"></i
               ></label>
             </a>
           </div>
-          <div class="course-learn__container">
-            <ul class="course-learn__list">
-              <li class="course-learn__item">
+          <div className="course-learn__container">
+            <ul className="course-learn__list">
+              <li className="course-learn__item">
                 <a
                   href="vocabularyFlashCard.html"
-                  class="content__box-course content__box-course--done content__box-course--chosen content__box-course-in-list"
+                  className="content__box-course content__box-course--done content__box-course--chosen content__box-course-in-list"
                 >
                   <i
-                    class="fa-solid fa-circle-check content__box-course__icon-done"
+                    className="fa-solid fa-circle-check content__box-course__icon-done"
                   ></i>
-                  <h3 class="content__box-course-category">Từ vựng:</h3>
+                  <h3 className="content__box-course-category">Từ vựng:</h3>
                   <span
-                    class="content__box-description content__box-description-in-list"
+                    className="content__box-description content__box-description-in-list"
                   >
                     FlashCard</span
                   >
                 </a>
               </li>
-              <li class="course-learn__item">
+              <li className="course-learn__item">
                 <a
                   href="multichoiceLearn.html"
-                  class="content__box-course content__box-course--done content__box-course-in-list"
+                  className="content__box-course content__box-course--done content__box-course-in-list"
                 >
                   <i
-                    class="fa-solid fa-circle-check content__box-course__icon-done"
+                    className="fa-solid fa-circle-check content__box-course__icon-done"
                   ></i>
-                  <h3 class="content__box-course-category">Luyện tập:</h3>
+                  <h3 className="content__box-course-category">Luyện tập:</h3>
                   <span
-                    class="content__box-description content__box-description-in-list"
+                    className="content__box-description content__box-description-in-list"
                   >
                     Trắc nghiệm từ vựng</span
                   >
                 </a>
               </li>
-              <li class="course-learn__item">
+              <li className="course-learn__item">
                 <a
                   href="findPairLearn.html"
-                  class="content__box-course content__box-course--done content__box-course-in-list"
+                  className="content__box-course content__box-course--done content__box-course-in-list"
                 >
                   <i
-                    class="fa-solid fa-circle-check content__box-course__icon-done"
+                    className="fa-solid fa-circle-check content__box-course__icon-done"
                   ></i>
-                  <h3 class="content__box-course-category">Luyện tập:</h3>
+                  <h3 className="content__box-course-category">Luyện tập:</h3>
                   <span
-                    class="content__box-description content__box-description-in-list"
+                    className="content__box-description content__box-description-in-list"
                   >
                     Tìm cặp</span
                   >
                 </a>
               </li>
-              <li class="course-learn__item">
+              <li className="course-learn__item">
                 <a
                   href="listenLearn.html"
-                  class="content__box-course content__box-course--done content__box-course-in-list"
+                  className="content__box-course content__box-course--done content__box-course-in-list"
                 >
                   <i
-                    class="fa-solid fa-circle-check content__box-course__icon-done"
+                    className="fa-solid fa-circle-check content__box-course__icon-done"
                   ></i>
-                  <h3 class="content__box-course-category">Luyện tập:</h3>
+                  <h3 className="content__box-course-category">Luyện tập:</h3>
                   <span
-                    class="content__box-description content__box-description-in-list"
+                    className="content__box-description content__box-description-in-list"
                   >
                     Nghe từ vựng</span
                   >
                 </a>
               </li>
-              <li class="course-learn__item">
+              <li className="course-learn__item">
                 <a
                   href="translateLearn.html"
-                  class="content__box-course content__box-course--done content__box-course-in-list"
+                  className="content__box-course content__box-course--done content__box-course-in-list"
                 >
                   <i
-                    class="fa-solid fa-circle-check content__box-course__icon-done"
+                    className="fa-solid fa-circle-check content__box-course__icon-done"
                   ></i>
-                  <h3 class="content__box-course-category">Luyện tập:</h3>
+                  <h3 className="content__box-course-category">Luyện tập:</h3>
                   <span
-                    class="content__box-description content__box-description-in-list"
+                    className="content__box-description content__box-description-in-list"
                   >
                     Dịch nghĩa / Điền từ</span
                   >
                 </a>
               </li>
-              <li class="course-learn__item">
+              <li className="course-learn__item">
                 <a
                   href="listenSpell.html"
-                  class="content__box-course content__box-course--done content__box-course-in-list"
+                  className="content__box-course content__box-course--done content__box-course-in-list"
                 >
                   <i
-                    class="fa-solid fa-circle-check content__box-course__icon-done"
+                    className="fa-solid fa-circle-check content__box-course__icon-done"
                   ></i>
-                  <h3 class="content__box-course-category">Luyện tập:</h3>
+                  <h3 className="content__box-course-category">Luyện tập:</h3>
                   <span
-                    class="content__box-description content__box-description-in-list"
+                    className="content__box-description content__box-description-in-list"
                   >
                     Nghe chính tả</span
                   >
@@ -113,112 +113,112 @@ export default function translateLearn(){
             </ul>
           </div>
         </div>
-        <div class="content-right">
-          <nav class="nav__content-right">
+        <div className="content-right">
+          <nav className="nav__content-right">
             <div
-              class="nav__content-header-right hide-on-mobile hide-on-tablet"
+              className="nav__content-header-right hide-on-mobile hide-on-tablet"
             >
-              <a href="" class="nav__content-link-right"
+              <a href="" className="nav__content-link-right"
                 >IELTS General Reading
               </a>
-              <i class="nav__content-right__icon fa-solid fa-chevron-right"></i>
-              <span class="nav__content-transition-right">Từ vựng TOEIC</span>
+              <i className="nav__content-right__icon fa-solid fa-chevron-right"></i>
+              <span className="nav__content-transition-right">Từ vựng TOEIC</span>
             </div>
-            <label class="hide" for="nav-mobile-course-checkbox"
-              ><i class="fa-solid fa-bars nav__icon"></i
+            <label className="hide" htmlFor="nav-mobile-course-checkbox"
+              ><i className="fa-solid fa-bars nav__icon"></i
             ></label>
           </nav>
           <input
             type="checkbox"
             id="nav-mobile-course-checkbox"
-            class="nav-mobile-course__input"
+            className="nav-mobile-course__input"
           />
-          <div class="nav__mobile-course hide">
-            <div class="flex__container header__container">
-              <a href="" class="content-left__header">IELTS General Reading</a>
-              <a href="" class="content-left__dropleft">
-                <label for="nav-mobile-course-checkbox"
-                  ><i class="content-left__icon fa-solid fa-angle-left"></i
+          <div className="nav__mobile-course hide">
+            <div className="flex__container header__container">
+              <a href="" className="content-left__header">IELTS General Reading</a>
+              <a href="" className="content-left__dropleft">
+                <label htmlFor="nav-mobile-course-checkbox"
+                  ><i className="content-left__icon fa-solid fa-angle-left"></i
                 ></label>
               </a>
             </div>
-            <div class="course-learn__container">
-              <ul class="course-learn__list">
-                <li class="course-learn__item">
+            <div className="course-learn__container">
+              <ul className="course-learn__list">
+                <li className="course-learn__item">
                   <a
                     href=""
-                    class="course-learn__link course-learn__link--chosen"
+                    className="course-learn__link course-learn__link--chosen"
                     >Từ vựng TOEIC</a
                   >
                 </li>
-                <li class="course-learn__item">
-                  <a href="" class="course-learn__link">
+                <li className="course-learn__item">
+                  <a href="" className="course-learn__link">
                     Phương pháp làm các dạng câu hỏi trong IELTS General Reading
                   </a>
                 </li>
-                <li class="course-learn__item">
-                  <a href="" class="course-learn__link">
+                <li className="course-learn__item">
+                  <a href="" className="course-learn__link">
                     Video chữa đề chi tiết
                   </a>
                 </li>
-                <li class="course-learn__item">
-                  <a href="" class="course-learn__link"> PRACTICE ZONE </a>
+                <li className="course-learn__item">
+                  <a href="" className="course-learn__link"> PRACTICE ZONE </a>
                 </li>
-                <li class="course-learn__item">
-                  <a href="" class="course-learn__link">
+                <li className="course-learn__item">
+                  <a href="" className="course-learn__link">
                     GT Practice Set 7 Test 1: Luyện tập từ vựng
                   </a>
                 </li>
               </ul>
             </div>
           </div>
-          <div class="content-right__container">
-            <div class="grid wide grid-wide-course-learn">
-              <div class="content__box">
-                <h3 class="translate__header">Sự xem trước, sự duyệt trước</h3>
-                <p class="translate__hint">
-                  Hint: = a chance to see something before it becomes generally
+          <div className="content-right__container">
+            <div className="grid wide grid-wide-course-learn">
+              <div className="content__box">
+                <h3 className="translate__header">Sự xem trước, sự duyệt trước</h3>
+                <p className="translate__hint">
+                  Hint: = a chance to see something behtmlFore it becomes generally
                   available
                 </p>
-                <div class="translate__input-container">
-                  <input type="text" class="translate__input" />
+                <div className="translate__input-container">
+                  <input type="text" className="translate__input" />
                 </div>
               </div>
-              <div class="multichoice__option">
-                <a href="" class="multichoice__option-link"
-                  ><i class="fa-solid fa-angle-left"></i> Câu trước</a
+              <div className="multichoice__option">
+                <a href="" className="multichoice__option-link"
+                  ><i className="fa-solid fa-angle-left"></i> Câu trước</a
                 >
-                <div class="multichoice__auto-container">
+                <div className="multichoice__auto-container">
                   <input type="checkbox" id="multichoice__checkbox" />
-                  <label for="multichoice__checkbox" class="multichoice__auto"
+                  <label htmlFor="multichoice__checkbox" className="multichoice__auto"
                     >Tự động chuyển câu</label
                   >
                 </div>
-                <a href="" class="multichoice__option-link"
-                  >Câu sau <i class="fa-solid fa-angle-right"></i
+                <a href="" className="multichoice__option-link"
+                  >Câu sau <i className="fa-solid fa-angle-right"></i
                 ></a>
               </div>
-              <div class="content__box">
-                <h3 class="multichoice__list-text">Danh sách bài tập:</h3>
-                <div class="multichoice__list-box">
+              <div className="content__box">
+                <h3 className="multichoice__list-text">Danh sách bài tập:</h3>
+                <div className="multichoice__list-box">
                   <a
                     href=""
-                    class="multichoice__list-number multichoice__list-number--chosen"
+                    className="multichoice__list-number multichoice__list-number--chosen"
                     >1</a
                   >
-                  <a href="" class="multichoice__list-number">2</a>
-                  <a href="" class="multichoice__list-number">3</a>
-                  <a href="" class="multichoice__list-number">4</a>
-                  <a href="" class="multichoice__list-number">5</a>
-                  <a href="" class="multichoice__list-number">6</a>
-                  <a href="" class="multichoice__list-number">7</a>
-                  <a href="" class="multichoice__list-number">8</a>
-                  <a href="" class="multichoice__list-number">9</a>
-                  <a href="" class="multichoice__list-number">10</a>
-                  <a href="" class="multichoice__list-number">11</a>
-                  <a href="" class="multichoice__list-number">12</a>
-                  <a href="" class="multichoice__list-number">13</a>
-                  <a href="" class="multichoice__list-number">14</a>
+                  <a href="" className="multichoice__list-number">2</a>
+                  <a href="" className="multichoice__list-number">3</a>
+                  <a href="" className="multichoice__list-number">4</a>
+                  <a href="" className="multichoice__list-number">5</a>
+                  <a href="" className="multichoice__list-number">6</a>
+                  <a href="" className="multichoice__list-number">7</a>
+                  <a href="" className="multichoice__list-number">8</a>
+                  <a href="" className="multichoice__list-number">9</a>
+                  <a href="" className="multichoice__list-number">10</a>
+                  <a href="" className="multichoice__list-number">11</a>
+                  <a href="" className="multichoice__list-number">12</a>
+                  <a href="" className="multichoice__list-number">13</a>
+                  <a href="" className="multichoice__list-number">14</a>
                 </div>
               </div>
             </div>
