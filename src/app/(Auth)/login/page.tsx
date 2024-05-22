5b95550879e6418c06efe673223bfedd98feb30d
Nguyễn Hoàng Phong <138206785+Pwindy2611@users.noreply.github.com>
2024-05-22 17:03:21 +0700

    login and register fix complete

--- src/app/(Auth)/login/page.tsx
+++ src/app/(Auth)/login/page.tsx
@@ -18,6 +18,29 @@ import {
   } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { LoginBody, LoginBodyType } from '@/schemaValidate/auth.schema'
+import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+const words = [
+  {
+    text: "Log",
+    className:" text-3xl max-xl:text-2xl",
+  },
+  {
+    text: "in",
+    className:" text-3xl max-xl:text-2xl",
+  },
+  {
+    text: "to",
+    className:" text-3xl max-xl:text-2xl",
+  },
+  {
+    text: "your",
+    className:" text-3xl max-xl:text-2xl",
+  },
+  {
+    text: "Account",
+    className: "text-3xl max-xl:text-2xl text-blue-500 dark:text-blue-500",
+  },
+]
 
 export default function Login() {
     const form = useForm<LoginBodyType>({
@@ -43,10 +66,7 @@ export default function Login() {
     return (
       <div className=" mx-10 flex items-center justify-center">
         <div className=" w-[600px] px-[60px] py-[30px] rounded-lg shadow-xl">
-          <p className=" text-base font-medium">
-            Đăng nhập ngay để bắt đầu trải nghiệm học tiếng Anh và luyện thi
-            TOEIC/IELTS hiệu quả cùng hàng trăm ngàn học viên mỗi ngày.
-          </p>
+          <TypewriterEffectSmooth words={words} />
           <Form {...form}>
             <form
               onSubmit={form.handleSubmit(handleLogin)}
@@ -84,14 +104,26 @@ export default function Login() {
                   </button>
             </form>
           </Form>
-          <Link href="" className=" text-primary-bg-color hover:bg-slate-100 hover:text-slate-400 transition duration-150 ease-in-out mt-3 block px-3 py-2 border-[1px] border-primary-bg-color rounded no-underline text-base font-medium text-center"
-                >Đăng nhập với Facebook</Link>
+          <div className=' m-auto w-4/5 flex items-center justify-center gap-3'>
+              <hr className=' w-full'></hr>
+              <span className=' text-lg font-normal text-slate-300'>Or</span>
+              <hr className='w-full'></hr>
+            </div>
+          <Link href="" className=" flex justify-center items-center gap-3 hover:bg-slate-100 transition duration-500 ease-in-out text-slate-400 mt-3 w-full px-3 py-2 border-[2px] border-slate-300 rounded no-underline text-base font-normal text-center"
+          >
+            <i className="fa-brands fa-facebook text-2xl text-blue-500"></i>
+            <span>Đăng nhập với Facebook</span></Link>
               <br />
-              <Link href="" className=" text-white hover:bg-red-700 transition duration-150 ease-in-out mt-3 block px-3 py-2 bg-red-500 rounded no-underline text-base font-medium text-center"
-                >Đăng nhập với Google</Link>
+              <Link href="" className="flex justify-center items-center gap-3 hover:bg-slate-100 transition duration-500 ease-in-out text-slate-400 mt-3 w-full px-3 py-2 border-[2px] border-slate-300 rounded no-underline text-base font-normal text-center"
+                >
+                  <i className="fa-brands fa-google text-2xl text-red-500"></i>
+                  <span>Đăng nhập với Google</span></Link>
           <br />
-          <Link href="/register" className="block mt-[20px] no-underline text-black text-base transition duration-500 ease-in-out hover:text-primary-bg-color"
-            >Bạn chưa là một thành viên? Đăng ký ngay!</Link>
+          <div className='flex items-center gap-1 justify-center mt-[20px] no-underline text-black text-base '>
+            <span>Bạn chưa là một thành viên?</span>
+            <Link href="/register" className="hover:underline transition duration-300 ease-in-out hover:text-primary-bg-color"
+            > Đăng ký ngay</Link>
+          </div>
         </div>
       </div>
     )
--- src/app/(Auth)/register/page.tsx
+++ src/app/(Auth)/register/page.tsx
@@ -17,6 +17,22 @@ import {
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { RegisterBody, RegisterBodyType } from '@/schemaValidate/auth.schema'
+import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+
+const words = [
+  {
+    text: "Create",
+    className:" text-3xl max-xl:text-2xl",
+  },
+  {
+    text: "your",
+    className:" text-3xl max-xl:text-2xl",
+  },
+  {
+    text: "Account",
+    className: "text-3xl max-xl:text-2xl text-blue-500 dark:text-blue-500",
+  },
+]
 
 
 export default function Register(){
@@ -46,10 +62,7 @@ export default function Register(){
     return(
         <div className="mx-10 flex items-center justify-center">
           <div className=" w-[600px] px-[60px] py-[30px] rounded-lg shadow-xl">
-            <p className=" text-base font-medium">
-              Đăng ký ngay để bắt đầu trải nghiệm học tiếng Anh và luyện thi
-              TOEIC/IELTS hiệu quả cùng hàng trăm ngàn học viên mỗi ngày.
-            </p>
+            <TypewriterEffectSmooth words={words} />
             <Form {...form}>
               <form
               className='flex flex-col'
@@ -124,14 +137,27 @@ export default function Register(){
                 Đăng ký
               </button>
             </form>
-            <div className=' w-4/5 m-auto h-[2px] bg-slate-300 mb-7'></div>
             </Form>
-            <Link href="" className=" text-primary-bg-color hover:bg-slate-100 hover:text-slate-400 transition duration-150 ease-in-out mt-3 block px-3 py-2 border-[1px] border-primary-bg-color rounded no-underline text-base font-medium text-center">Đăng ký với Facebook</Link>
-            <br />
-            <Link href="" className=" text-white hover:bg-red-700 transition duration-150 ease-in-out mt-3 block px-3 py-2 bg-red-500 rounded no-underline text-base font-medium text-center">Đăng ký với Google</Link>
-            <br />
-            <Link href="/login" className=" block mt-[20px] no-underline text-black text-base transition duration-500 ease-in-out hover:text-primary-bg-color"
-              > Đã có tài khoản? Đăng nhập ngay!</Link>
+            <div className=' m-auto w-4/5 flex items-center justify-center gap-3'>
+              <hr className=' w-full'></hr>
+              <span className=' text-lg font-normal text-slate-300'>Or</span>
+              <hr className='w-full'></hr>
+            </div>
+          <Link href="" className=" flex justify-center items-center gap-3 hover:bg-slate-100 transition duration-500 ease-in-out text-slate-400 mt-3 w-full px-3 py-2 border-[2px] border-slate-300 rounded no-underline text-base font-normal text-center"
+          >
+            <i className="fa-brands fa-facebook text-2xl text-blue-500"></i>
+            <span>Đăng kí với Facebook</span></Link>
+              <br />
+              <Link href="" className="flex justify-center items-center gap-3 hover:bg-slate-100 transition duration-500 ease-in-out text-slate-400 mt-3 w-full px-3 py-2 border-[2px] border-slate-300 rounded no-underline text-base font-normal text-center"
+                >
+                  <i className="fa-brands fa-google text-2xl text-red-500"></i>
+                  <span>Đăng kí với Google</span></Link>
+          <br />
+          <div className='flex items-center gap-1 justify-center mt-[20px] no-underline text-black text-base '>
+            <span>Bạn đã có tài khoản?</span>
+            <Link href="/register" className="hover:underline transition duration-300 ease-in-out hover:text-primary-bg-color"
+            > Đăng nhập ngay</Link>
+          </div>
           </div>
         </div>
     )
