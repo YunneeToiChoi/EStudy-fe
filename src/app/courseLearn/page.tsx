7ddee0cc7829bc7562f216faae1fa69edfe84279
Nguyễn Hoàng Phong <138206785+Pwindy2611@users.noreply.github.com>
2024-05-24 22:52:13 +0700

    half course learn

--- src/app/courseLearn/page.tsx
+++ src/app/courseLearn/page.tsx
@@ -2,56 +2,56 @@ import "./courseLearn.css"
 export default function CourseLearn(){
     return(
         <div className="content__container">
-        <input type="checkbox" id="content__checkbox" className="content__input" />
-        <div className="content-left hide-on-mobile hide-on-tablet">
-          <div className="flex__container header__container">
-            <a href="" className="content-left__header">IELTS General Reading</a>
-            <a href="" className="content-left__dropleft">
+        <input type="checkbox" id="content__checkbox" className=" hidden" />
+        <div className=" fixed z-[2] bg-white w-[300px] translate-x-0">
+          <div className=" flex px-2 py-5 bg-nav-hover-text-color items-center justify-between">
+            <a href="" className=" text-xl no-underline text-white">IELTS General Reading</a>
+            <a href="" className=" text-inherit decoration-inherit">
               <label htmlFor="content__checkbox"
-                ><i className="content-left__icon fa-solid fa-angle-left"></i
+                ><i className=" text-white text-xl cursor-pointer fa-solid fa-angle-left"></i
               ></label>
             </a>
           </div>
-          <div className="course-learn__container">
-            <ul className="course-learn__list">
+          <div className=" border-r-[1px] border-r-[#e0e0e0]">
+            <ul className=" list-none">
               <li className="course-learn__item">
-                <a href="" className="course-learn__link course-learn__link--chosen"
+                <a href="" className=" border-l-4 border-l-transparent hover:border-l-nav-hover-text-color hover:bg-tag-search-bg-color block px-5 py-4 no-underline text-base text-black transition duration-300 ease-in-out"
                   >Bài học thử</a
                 >
               </li>
               <li className="course-learn__item">
-                <a href="courseVocabulary.html" className="course-learn__link">
+                <a href="courseVocabulary.html" className="border-l-4 border-l-transparent hover:border-l-nav-hover-text-color hover:bg-tag-search-bg-color block px-5 py-4 no-underline text-base text-black transition duration-300 ease-in-out">
                   Từ vựng TOEIC
                 </a>
               </li>
               <li className="course-learn__item">
-                <a href="" className="course-learn__link">
+                <a href="" className="border-l-4 border-l-transparent hover:border-l-nav-hover-text-color hover:bg-tag-search-bg-color block px-5 py-4 no-underline text-base text-black transition duration-300 ease-in-out">
                   Video chữa đề chi tiết
                 </a>
               </li>
               <li className="course-learn__item">
-                <a href="" className="course-learn__link"> PRACTICE ZONE </a>
+                <a href="" className="border-l-4 border-l-transparent hover:border-l-nav-hover-text-color hover:bg-tag-search-bg-color block px-5 py-4 no-underline text-base text-black transition duration-300 ease-in-out"> PRACTICE ZONE </a>
               </li>
               <li className="course-learn__item">
-                <a href="" className="course-learn__link">
+                <a href="" className="border-l-4 border-l-transparent hover:border-l-nav-hover-text-color hover:bg-tag-search-bg-color block px-5 py-4 no-underline text-base text-black transition duration-300 ease-in-out">
                   GT Practice Set 7 Test 1: Luyện tập từ vựng
                 </a>
               </li>
             </ul>
           </div>
         </div>
-        <div className="content-right">
-          <nav className="nav__content-right">
+        <div className=" ml-[300px] bg-[#D4F1F4] min-h-[1200px]">
+          <nav className=" fixed bg-white border-b-[1px] border-b-course-border-color w-full p-4 flex items-center justify-between h-[60px]">
             <div
-              className="nav__content-header-right hide-on-mobile hide-on-tablet"
+              className=" max-lg:hidden flex items-center"
             >
-              <a href="" className="nav__content-link-right"
+              <a href="" className=" no-underline text-nav-hover-text-color text-sm"
                 >IELTS General Reading
               </a>
-              <i className="nav__content-right__icon fa-solid fa-chevron-right"></i>
-              <span className="nav__content-transition-right">Bài học thử</span>
+              <i className=" text-sm px-1 text-nav-hover-text-color fa-solid fa-chevron-right"></i>
+              <span className="no-underline text-nav-hover-text-color text-sm font-medium">Bài học thử</span>
             </div>
-            <label className="hide" htmlFor="nav-mobile-course-checkbox"
+            <label className="lg:hidden" htmlFor="nav-mobile-course-checkbox"
               ><i className="fa-solid fa-bars nav__icon"></i
             ></label>
           </nav>
