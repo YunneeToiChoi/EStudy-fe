daaf575be947399c8d962e7316dfd2a11633fdf3
Nguyễn Hoàng Phong <138206785+Pwindy2611@users.noreply.github.com>
2024-07-23 22:19:03 +0700

    fix layout

--- src/app/components/course/bodyMainCourse.tsx
+++ src/app/components/course/bodyMainCourse.tsx
@@ -1,6 +1,6 @@
  export default function BodyMainCourse({ children }: { children: React.ReactNode }){
     return(
-        <div className="peer-checked/checkboxTranslate:ml-[30px] transition duration-500 ease-linear animate-out lg:ml-[300px] bg-[#f8f9fa] min-h-[800px]">
+        <div className="peer-checked/checkboxTranslate:ml-[30px] transition duration-500 ease-linear animate-out lg:ml-[300px] bg-[#f8f9fa] min-h-[1200px]">
          {children}
         </div>
     )
